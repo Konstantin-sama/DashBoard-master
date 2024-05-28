fa6a4bc01559a60d54f00553498f6b286c62eeb8
Konstantin <mine@gmail.com>
2024-05-28 10:40:48 +0300

    commit push

---
+++ CallBoard/CallBoard/celery.py
@@ -0,0 +1,14 @@
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Callboard.settings')
+#
+# app = Celery('Callboard')
+# app.config_from_object('django.conf:settings', namespace='CELERY')
+#
+# app.autodiscover_tasks()
+
+# app.conf.beat_schedule = {
+#     'send_weekly_posts': {
+#         'task': 'board.tasks.send_email_weekly_posts',
+#         'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
+#         'args': (),
+#     },
+# }
